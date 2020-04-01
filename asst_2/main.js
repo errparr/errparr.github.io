84e961510615cd8a8b440b846d1cd0a8b9de297b
errparr <60356488+errparr@users.noreply.github.com>
2020-04-01 17:05:27 -0400

    assn 2

--- asst_2/main.js
+++ asst_2/main.js
@@ -4,15 +4,36 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+//This link helped us tremendously with getting the base of our code//
+//https://github.com/mdn/learning-area/tree/master/javascript/building-blocks/gallery
+//Followed along the MDN and their example of the image gallery
+
 /* Looping through images */
-for(let i = 1; i <= 5; i++) {
-    const newImage = document.createElement('img');
+for(let i = 1; i <= 5; i++) 
+{
+    const newImage=document.createElement('img');
     newImage.setAttribute('src', 'images/pic' + i + '.jpg');
     thumbBar.appendChild(newImage);
-    newImage.onclick = function(e) 
+    newImage.onclick=function(e) 
     {
       displayedImage.src = e.target.src;
-    }
+    } 
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function() 
+{
+  const btnClass = btn.getAttribute('class');
+  if(btnClass==='dark') //switching between light and dark
+  {
+    btn.setAttribute('class','light');
+    btn.textContent='Lighten';
+    overlay.style.backgroundColor='rgba(0,0,0,0.5)';
+  } 
+  else 
+  {
+    btn.setAttribute('class','dark');
+    btn.textContent='Darken';
+    overlay.style.backgroundColor='rgba(0,0,0,0)';
+  }
+}
